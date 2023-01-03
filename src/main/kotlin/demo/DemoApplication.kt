f6d7356d50a69f8181cd9621ab03517060bf19c0
Phil123095 <68194774+Phil123095@users.noreply.github.com>
2023-01-03 13:01:24 +0100

    Fixed application.properties inputs to correctly set up SQL db.

--- src/main/kotlin/demo/DemoApplication.kt
+++ src/main/kotlin/demo/DemoApplication.kt
@@ -3,14 +3,14 @@ package demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.annotation.Id
-import org.springframework.data.relational.core.mapping.Table
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
 
 
 @SpringBootApplication
@@ -20,23 +20,20 @@ fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
 
-// Messages table created in SQL schema. Table structure matches Message class:
-/*
-	CREATE TABLE IF NOT EXISTS messages (
-	  id                     VARCHAR(60)  DEFAULT RANDOM_UUID() PRIMARY KEY, <- Creates unique ID
-	  text                   VARCHAR      NOT NULL <- Can't be null (as specified in data class below)
-	);
+/* @RestController = "extension" of @Controller notation. Tells Spring Framework that the class serves as a Controller
+	in Spring MVC.
  */
-@Table("MESSAGES")
-data class Message(@Id val id: String?, val text: String)
+@RestController
+class MessageResource(val service: MessageService) {
+	@GetMapping("/messages")
+	fun index(): List<Message> = service.findMessages()
 
-// Spring Data Repository API to access DB.
-// @Repository = Classes in the PERSISTENCE layer (DB repository)
-interface MessageRepository: CrudRepository<Message, String>{
-	// When calling the findMessages() method on instance of this, it will exec. the query.
-	@Query("select * from messages")
-	fun findMessages(): List<Message>
+	@PostMapping("/")
+	fun post(@RequestBody message: Message) {
+		service.post(message)
+	}
 }
+
 // @Service = Annotation marking class as a SERVICE PROVIDER (= providing some business functionalities).
 // = Classes residing in SERVICE LAYER.
 @Service
@@ -49,17 +46,21 @@ class MessageService(val db: MessageRepository) {
 	}
 }
 
-
-/* @RestController = "extension" of @Controller notation. Tells Spring Framework that the class serves as a Controller
-	in Spring MVC.
- */
-@RestController
-class MessageResource(val service: MessageService) {
-	@GetMapping("/")
-	fun index(): List<Message> = service.findMessages()
-
-	@PostMapping
-	fun post(@RequestBody message: Message) {
-		service.post(message)
-	}
+// Spring Data Repository API to access DB.
+// @Repository = Classes in the PERSISTENCE layer (DB repository)
+interface MessageRepository: CrudRepository<Message, String>{
+	// When calling the findMessages() method on instance of this, it will exec. the query.
+	@Query("select * from messages")
+	fun findMessages(): List<Message>
 }
+
+// Messages table created in SQL schema. Table structure matches Message class:
+/*
+	CREATE TABLE IF NOT EXISTS messages (
+	  id                     VARCHAR(60)  DEFAULT RANDOM_UUID() PRIMARY KEY, <- Creates unique ID
+	  text                   VARCHAR      NOT NULL <- Can't be null (as specified in data class below)
+	);
+ */
+@Table("MESSAGES")
+data class Message(@Id val id: String?, val text: String)
+
